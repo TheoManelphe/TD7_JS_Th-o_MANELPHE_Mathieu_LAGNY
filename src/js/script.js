1469ea0bdeb501d4b046614c4718ef6f9bc3cdd6
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2020-03-27 16:16:43 +0100

    creation Livre en cours

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 let adherents = new Adherent();
 let livres = new Livres();
 
+let ajouterLivre = document.getElementById("ajouterLivre");
+ajouterLivre.addEventListener("click",function(){livres.ajouter();});
+
+//La vie au XIVe siecle
